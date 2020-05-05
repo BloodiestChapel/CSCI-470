85ff9463cecc64665a62d73297a8fdb02178a61e
Dillon Chappell <55209913+BloodiestChapel@users.noreply.github.com>
2020-05-05 00:25:57 -0500

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,10 +1,11 @@
 public class EvenOdd {
 
     public static void main(String[] args) {
-
-        if(args[0] % 2 == 0)
-            System.out.println(args[0] + " is even");
+        int num = Integer.parseInt(args[0]);
+        
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
         else
-            System.out.println(args[0] + " is odd");
+            System.out.println(num + " is odd");
     }
 }
