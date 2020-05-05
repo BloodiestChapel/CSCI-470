bb87fa47b1632dea780619a97caac7898c66f2ab
Dillon Chappell <55209913+BloodiestChapel@users.noreply.github.com>
2020-05-05 00:20:54 -0500

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,17 +1,10 @@
-import java.util.Scanner;
-
 public class EvenOdd {
 
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int num = reader.nextInt();
-
-        if(num % 2 == 0)
-            System.out.println(num + " is even");
+        if(args[0] % 2 == 0)
+            System.out.println(args[0] + " is even");
         else
-            System.out.println(num + " is odd");
+            System.out.println(args[0] + " is odd");
     }
 }
