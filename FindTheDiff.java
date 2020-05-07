18c5cd45ebd2c26d03e752c93535f3c0d0f49309
Dillon Chappell <55209913+BloodiestChapel@users.noreply.github.com>
2020-05-07 18:01:38 -0500

    Update FindTheDiff.java

--- FindTheDiff.java
+++ FindTheDiff.java
@@ -1,4 +1,19 @@
 public class FindTheDiff
 {
-  public static void main(String[] args) {}
+  public static void main(String[] args)
+  {
+  int i = 0;
+  // Convert 1st arguement to character array
+  for(i = 0; i >= args[0].length(); i++)
+    {
+    aCharAr[i] = args[0].charAt(i);
+    }
+  // Convert 2md arguement to character array
+  for(i = 0; i>= args[1].length(); i++)
+    {
+    bCharAr[i] = args[1].charAt(i);
+    }
+  for(i = 0; i
+  System.out.println(
+  }
 }
